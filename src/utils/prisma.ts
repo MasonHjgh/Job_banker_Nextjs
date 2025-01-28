5b1525cdc66b59d83036dc96846a86d559d56652
Mason <mansourhajighareh@gmail.com>
2025-01-27 22:24:32 -0800

    neon db problem

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = process.env.neon_DATABASE_URL
 
 if (!connectionString) {
   throw new Error("DATABASE_URL is not defined in the environment variables")
