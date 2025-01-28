7d9277c1f811122b7ee8ab3530f5842b1a7e3fe1
Mason <mansourhajighareh@gmail.com>
2025-01-27 22:14:25 -0800

    type error fxied

--- src/features/react-hook-forms/JobTableForm.tsx
+++ src/features/react-hook-forms/JobTableForm.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, type SubmitHandler } from "react-hook-form"
 import { StatusDropdownData } from "Resources/DropDownsData"
 
 type JobFormInputs = {
